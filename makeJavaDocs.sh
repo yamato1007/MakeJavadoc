7a1382a89ab4f948ae8d1e570548f7126b7814cc
yamato_tanaka <a.niger.333141592653589793@gmail.com>
2015-12-19 00:54:57 +0900

    Javadocを生成するシェルスクリプトの作成

---
+++ makeJavaDocs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#javadocを作成するディレクトリ
+DIRNAME=documents
+#.javaファイルの一覧が書かれたファイルの名前
+JAVAFILESNAME=javaFiles.txt
+#.javaファイルの格納されているディレクトリ
+SRCDIR=src
+
+#javadocのディレクトリの削除と作成
+rm -rf "${DIRNAME}"
+mkdir $DIRNAME
+
+#javaファイル一覧のファイル作成
+find $SRCDIR -name '*.java' -print > $JAVAFILESNAME
+
+#javadocの作成
+javadoc -d $DIRNAME @"${JAVAFILESNAME}"
